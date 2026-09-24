b9069e37c771a7afc6cb465a8c5619b8ef41bc8e
Naman Shah <209906685+cdtnshah4@users.noreply.github.com>
2026-09-24 12:37:50 +0530

    215. Kth Largest Element in an Array

---
+++ 215.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int x : nums) {
+            minHeap.offer(x);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
